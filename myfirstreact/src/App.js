89909aeafa81841e316e1c1f71d8bc027aa03bce
ronieldegozo <ronieldegozo@gmail.com>
2022-11-12 10:04:38 -0800

    Using set state and this state

--- myfirstreact/src/App.js
+++ myfirstreact/src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Component} from 'react'; //Render
+
+
+class App extends Component{ //
+
+  constructor(){
+    super();
+    this.state = {
+      fullname: 'Rogine'
+    }
+  }
+
+  render(){ //render 
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi {this.state.fullname}
+          </p>
+        <button onClick={ ()=> {
+          this.setState({fullname: 'Roniel'}); //set state update the state into different object
+          console.log(this.state);
+        }}>
+          Change my name
+        </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
